2827a0fb9cecfeaf341930d1f6cad082f12009f5
subh <subhajitchaudhury05@gmail.com>
2025-11-16 02:29:36 +0530

    setup with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="text-5xl font-bold text-blue-500">Fuck you</Text>
+    </View>
+  );
+}
