ff7b2c4f4e53f8e68ab8fdfa98f728bbcef12e96
subh <subhajitchaudhury05@gmail.com>
2025-11-16 02:59:46 +0530

    navigation and routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -5,7 +6,9 @@ export default function Index() {
     <View
       className="flex-1 justify-center items-center"
     >
-      <Text className="text-5xl font-bold text-primary">Fuck you</Text>
+      <Text className="text-5xl font-bold text-primary">Welcome !!!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
